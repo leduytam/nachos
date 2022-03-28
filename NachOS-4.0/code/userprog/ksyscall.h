3b21682c55b68f24b92e5732a3f955f79ac4571b
tamle <leduytam600@gmail.com>
2022-03-28 17:11:29 +0700

    add read num

--- NachOS-4.0/code/userprog/ksyscall.h
+++ NachOS-4.0/code/userprog/ksyscall.h
@@ -13,7 +13,9 @@
 
 #include "kernel.h"
 #include "synchconsole.h"
-#include <vector>
+#include <cstring>
+#include <string>
+#include <climits>
 
 void SysHalt()
 {
@@ -27,12 +29,51 @@ int SysAdd(int op1, int op2)
 
 int SysReadNum()
 {
-    return 0;
+    string buffer;
+
+    while (true)
+    {
+        char c = kernel->synchConsoleIn->GetChar();
+
+        if (c == ' ' || c == '\n' || c == EOF || c == ' ')
+            break;
+
+        buffer.push_back(c);
+    }
+
+    if (buffer.length() == 0 || (buffer.length() == 1 && buffer[0] == '-'))
+        return 0;
+
+    if (buffer == "-2147483648")
+        return INT_MIN;
+
+    bool isNegative = buffer[0] == '-';
+    bool isLeadingZero = true;
+    long long result = 0;
+
+    for (int i = isNegative; i < buffer.length(); i++)
+    {
+        if (!isdigit(buffer[i]))
+            return 0;
+
+        if (isLeadingZero && buffer[i] != '0')
+            isLeadingZero = false;
+
+        if (!isLeadingZero)
+        {
+            result = result * 10 + (buffer[i] - '0');
+
+            if (result > INT_MAX)
+                return 0;
+        }
+    }
+
+    return isNegative ? -result : result;
 }
 
 void SysPrintNum(int number)
 {
-    vector<char> buffer;
+    string buffer;
 
     if (number == 0)
         buffer.push_back('0');
@@ -67,22 +108,74 @@ int SysRandomNum()
     return random();
 }
 
-void SysReadString(char buffer[], int length);
+void SysReadString(char buffer[], int length)
+{
+    buffer[length] = '\0';
 
-void SysPrintString(char buffer[]);
+    for (int i = 0; i < length; i++)
+        buffer[i] = kernel->synchConsoleIn->GetChar();
+}
 
-int SysCreate(char* name);
+void SysPrintString(char buffer[])
+{
+    int len = strlen(buffer);
 
-OpenFileId SysOpen(char* name);
+    for (int i = 0; i < len; i++)
+        kernel->synchConsoleOut->PutChar(buffer[i]);
+}
 
-int SysClose(OpenFileId id);
+int SysCreate(char* name)
+{
+    if (name == NULL)
+    {
+        DEBUG(dbgSys, "SysCreate: Not enough memory to create file");
+        return -1;
+    }
 
-int SysRead(char* buffer, int size, OpenFileId id);
+    if (strlen(name) == 0)
+    {
+        DEBUG(dbgSys, "SysCreate: File name is empty\n");
+    }
 
-int SysWrite(char* buffer, int size, OpenFileId id);
+    if (!kernel->fileSystem->Create(name))
+    {
+        DEBUG(dbgSys, "SysCreate: Failed to create " << name << "\n");
+        return -1;
+    }
 
-int SysSeek(int position, OpenFileId id);
+    DEBUG(dbgSys, "SysCreate: Created " << name << "\n");
 
-int SysRemove(char* name);
+    return 0;
+}
+
+OpenFileId SysOpen(char* name)
+{
+    return 0;
+}
+
+int SysClose(OpenFileId id)
+{
+    return 0;
+}
+
+int SysRead(char* buffer, int size, OpenFileId id)
+{
+    return 0;
+}
+
+int SysWrite(char* buffer, int size, OpenFileId id)
+{
+    return 0;
+}
+
+int SysSeek(int position, OpenFileId id)
+{
+    return 0;
+}
+
+int SysRemove(char* name)
+{
+    return 0;
+}
 
 #endif /* ! __USERPROG_KSYSCALL_H__ */
